8b898047638d43b8bcca23528afb8b00645a7fd1
Alexandros Diamantonis <asdiamantonis@gmail.com>
2022-05-19 08:10:43 +0200

    0.0.2-alpha.2 build

--- dist/mathform.cjs
+++ dist/mathform.cjs
@@ -1,7 +1,7 @@
 /*!
 @fileoverview mathform - A collection of mathematical formulas, constants and algorithms
 @author Alexandros Diamantonis <asdiamantonis@gmail.com>
-@version 0.0.2-alpha.1
+@version 0.0.2-alpha.2
 
 Copyright (c) 2022, Alexandros Diamantonis <asdiamantonis@gmail.com>.
 
